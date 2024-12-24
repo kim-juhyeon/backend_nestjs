766024c7e363ef3181374a29f87425bb64d82843
jju <totkfka4@gmail.com>
2024-12-24 22:39:11 +0900

    feat : pnpm i @nestjs/config joi @nestjs/typeorm typeorm pg

--- src/movie/dto/update-movie.dto.ts
+++ src/movie/dto/update-movie.dto.ts
@@ -34,28 +34,6 @@ import {
 } from 'class-validator';
 import { register } from 'module';
 
-@ValidatorConstraint()
-class PasswordValidator implements ValidatorConstraintInterface {
-  validate(
-    value: any,
-    validationArguments?: ValidationArguments,
-  ): boolean | Promise<boolean> {
-    return value.length > 4 && value.length < 8;
-  }
-  defaultMessage?(validationArguments?: ValidationArguments): string {
-    return '비밀번호의 길이는 4~8자 여야 합니다.입력된비밀번호 ($value)';
-  }
-}
-function IsPasswordValid(validationOptions?: ValidationOptions) {
-  return function (object: Object, propertyName: string) {
-    registerDecorator({
-      target: object.constructor,
-      propertyName,
-      options: validationOptions,
-      validator: PasswordValidator,
-    });
-  };
-}
 export class UpdateMovieDto {
   @IsNotEmpty()
   @IsOptional()
@@ -64,30 +42,4 @@ export class UpdateMovieDto {
   @IsOptional()
   @IsNotEmpty()
   genre?: string;
-
-  // null / undefined
-  //   @IsDefined()
-  //   @Equals('code factory')
-  // @NotEquals('code factory')
-  //   @IsEmpty()
-  //   @IsBoolean()
-  //   @IsString()
-  //   @IsNumber()
-  //   @IsInt()
-  //   @IsArray()
-  //   @IsDateString()
-  //   @IsDivisibleBy(5)
-  //   @IsNegative()
-  //   @Min(100)
-  //   @Max(100)
-  //   @Contains('code factory')
-  //   @NotContains('code factory')
-  //   @IsAlphanumeric() ///TODO 알파벳과 숫자만
-  //   @IsCreditCard()
-  //   @IsHexColor()
-  //   @MaxLength(16)
-  //   @IsUUID()
-  //   @IsLatLong()
-  @IsPasswordValid()
-  test: string;
 }
--- src/movie/entity/movie.entity.ts
+++ src/movie/entity/movie.entity.ts
@@ -1,5 +1,30 @@
+import { Exclude, Expose, Transform } from 'class-transformer';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  VersionColumn,
+} from 'typeorm';
+
+@Entity()
 export class Movie {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   title: string;
+
+  @Column()
   genre: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @VersionColumn()
+  version: number;
 }
